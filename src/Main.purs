7ac6b94e0a79a31d3324b76b9c0ce9eb0ba8a330
justinwoo <moomoowoo@gmail.com>
2019-04-14 20:01:57 +0300

    init

---
+++ src/Main.purs
@@ -0,0 +1,34 @@
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Console (log)
+import Prim.Row as Row
+import Record as Record
+import Type.Prelude (class TypeEquals, SProxy(..), from)
+
+class MyClass a where
+  method :: a -> String
+
+-- -- with Prim type constructor Record
+-- instance myClassRecord ::
+--   ( Row.Cons "hi" String tail row
+--   , TypeEquals (Record (hi :: String | tail)) (Record row)
+--   ) => MyClass (Record row) where
+--   method r = Record.get (SProxy :: _ "hi") r'
+--     where
+--       r' = from r
+
+-- with brace syntax sugar
+instance myClassRecord ::
+  ( Row.Cons "hi" String tail row
+  , TypeEquals { hi :: String | tail } { | row }
+  ) => MyClass { | row } where
+  method r = Record.get (SProxy :: _ "hi") r'
+    where
+      r' = from r
+
+main :: Effect Unit
+main = do
+  log "Hello sailor!"
